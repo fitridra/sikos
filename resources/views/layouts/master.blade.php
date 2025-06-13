ad4afffe972696eb40924f5456a5513cd069a08f
Fitri Dwi <fitridwirahma11@gmail.com>
2025-06-14 00:01:58 +0700

    change title project

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>sharehome</title>
+    <title>HAY Property</title>
     <link rel="shortcut icon" type="image/png" href="{{asset('assets/images/logos/favicon1.png')}}" />
     <link rel="stylesheet" href="{{asset('assets/css/styles.min.css')}}" />
 </head>
