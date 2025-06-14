1606a19661b1d2121f90e7e1adfd083251fe9aad
Fitri Dwi <fitridwirahma11@gmail.com>
2025-06-15 00:02:26 +0700

    no hover in dropdown user

--- resources/views/layouts/includes/_navbar.blade.php
+++ resources/views/layouts/includes/_navbar.blade.php
@@ -18,11 +18,11 @@
                     </a>
                     <div class="dropdown-menu dropdown-menu-end dropdown-menu-animate-up" aria-labelledby="drop2">
                         <div class="message-body">
-                            <a href="javascript:void(0)" class="d-flex align-items-center gap-2 dropdown-item">
+                            <div class="d-flex align-items-center gap-2"
+                                style="padding: 0.5rem 1rem; text-decoration: none; color: inherit; background-color: transparent; cursor: default;">
                                 <i class="ti ti-user fs-6"></i>
-                                <p class="mb-0 fs-3">{{ Auth::user()->name }}</p>
-                            </a>
-                            {{-- <a href="{{ route('logout') }}" class="btn btn-outline-primary mx-3 mt-2 d-block">Logout</a> --}}
+                                <p class="mb-0 fs-3" style="margin: 0;">{{ Auth::user()->name }}</p>
+                            </div>
                             <form method="POST" action="{{ route('logout') }}" class="mx-3 mt-2 d-block">
                                 @csrf
                                 <button type="submit" class="btn btn-outline-primary w-100">
