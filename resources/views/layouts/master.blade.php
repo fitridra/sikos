e3823697677e97b70be41f04803602091ae1d47d
Fitri Dwi <fitridwirahma11@gmail.com>
2025-06-22 20:48:17 +0700

    logo

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>HAY Property</title>
-    <link rel="shortcut icon" type="image/png" href="{{asset('assets/images/logos/favicon1.png')}}" />
+    <link rel="shortcut icon" type="image/png" href="{{asset('assets/images/logos/favicon.png')}}" />
     <link rel="stylesheet" href="{{asset('assets/css/styles.min.css')}}" />
 </head>
 
