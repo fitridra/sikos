68e16dfbd7f373d107a263fd4e62ebd1f33a5569
Fitri Dwi <fitridwirahma11@gmail.com>
2025-06-27 00:06:01 +0700

    timezone bug fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Config;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +23,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
+        date_default_timezone_set(Config::get('app.timezone'));
+
     }
 }
